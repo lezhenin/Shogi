ff56515b79bf4d060ec0459520ddd323e62b9491
lezhenin <yurij.lezhenin@yandex.ru>
2016-02-28 13:24:08 +0300

    Update GameApi.h

--- sources/Shogi/GameCore/GameApi.h
+++ sources/Shogi/GameCore/GameApi.h
@@ -10,8 +10,10 @@ public:
    virtual void initGame()=0;
    virtual void pickPiece()=0;
    virtual void movePiece()=0;
+   virtual void promotePiece()=0;
+   virtual void dropPiece()=0;
    virtual void getBoard()=0;
-   virtual bool isFinish()=0;
+   virtual void getGameStatus()=0;
 };
 
 #endif // GAMECORE_H
