43b34ee204bf8ed2097e5216ac1f972d88115e47
lezhenin <yurij.lezhenin@yandex.ru>
2016-03-02 23:40:16 +0300

    rename project

--- sources/Shogi/ShogiCore/ShogiAPI.h
+++ sources/Shogi/ShogiCore/ShogiAPI.h
@@ -1,14 +1,14 @@
 #ifndef GAMECORE_H
 #define GAMECORE_H
 
-
+/// Игровой интерфес
 class GameAPI
 {
 
 public:
 
    virtual void initGame()=0;
-   virtual void pickPiece()=0;
+   virtual void pickPiece()=0; 
    virtual void movePiece()=0;
    virtual void promotePiece()=0;
    virtual void dropPiece()=0;
