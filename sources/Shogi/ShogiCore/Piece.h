ea8618083aef42706ef405f31c98aad4dfff041e
lezhenin <yurij.lezhenin@yandex.ru>
2016-03-12 21:54:57 +0300

    add commentaries for doxygen

--- sources/Shogi/ShogiCore/Piece.h
+++ sources/Shogi/ShogiCore/Piece.h
@@ -4,26 +4,38 @@
 #include "Square.h"
 
 class Square;
-/// Модель фигуры
+
+///Модель фигуры
 class Piece
 {
 public:
 
+    ///Конструктор
     Piece(PieceType pt, Player pl, Square *sq=nullptr);
 
+    ///Метод, позволяющий получить тип фигуры
     PieceType getType() const;
+    ///Метод, позволяющий получить игрока (сторону), которому принадлежит фигура
     Player getPlayer() const;
+    ///Метод, позволяющий получить позицию фигуры
     Position &getPosition() const;
 
+    ///Метод, позволяющий узнать была ли перевернута фигура
     inline bool wasPromoted() const;
+    ///Метод, позволяющий узнать может ли фигура быть первернута
     inline bool canBePromoted() const;
 
+    ///Метод, позволяющий перевернуть фигуру
     void promote();
+    ///Метод, позволяющий отменить переворот фигуры
     void unPromote();
 
+    ///Метод, позволяющий установить указатель на клетку доски, на которой находится фигура
     void setSquare(Square *sq);
+    ///Метод, позволяющий установить игрока (сторону), котому принадлежит фигура
     void setPlayer(Player pl);
 
+    ///Деструктор
     ~Piece();
 
 private:
