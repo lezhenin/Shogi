dce27ab9b2778c9fb6649040b8357a778490204d
lezhenin <yurij.lezhenin@yandex.ru>
2016-03-25 18:00:53 +0300

    add test for checkShah method

--- sources/ShogiCoreTest/tst_ShogiCoreModuleTest.cpp
+++ sources/ShogiCoreTest/tst_ShogiCoreModuleTest.cpp
@@ -27,17 +27,19 @@ ShogiCoreModuleTest::ShogiCoreModuleTest()
 void ShogiCoreModuleTest::mementoTest() {
     Piece *p1 = new Piece(Rook, Sente);
     Piece *p2 = new Piece(King, Gote);
-    Shogi game;
-    game.getBoard().setPiece(p1, Position(2, 2));
-    game.getBoard().setPiece(p2, Position(4, 5));
-    BoardMemento bm(&game.getBoard());
-    game.getBoard().removePiece(Position(2, 2));
-    game.getBoard().removePiece(Position(4, 5));
-    QVERIFY(game.getBoard().getPiece(Position(2, 2)) == nullptr);
-    QVERIFY(game.getBoard().getPiece(Position(4, 5)) == nullptr);
+    Board board;
+    board.setPiece(p1, Position(2, 2));
+    board.setPiece(p2, Position(4, 5));
+    BoardMemento bm(&board);
+    board.removePiece(Position(2, 2));
+    board.removePiece(Position(4, 5));
+    QVERIFY(board.getPiece(Position(2, 2)) == nullptr);
+    QVERIFY(board.getPiece(Position(4, 5)) == nullptr);
     bm.restore();
-    QVERIFY(game.getBoard().getPiece(Position(2, 2)) == p1);
-    QVERIFY(game.getBoard().getPiece(Position(4, 5)) == p2);
+    QVERIFY(board.getPiece(Position(2, 2)) == p1);
+    QVERIFY(board.getPiece(Position(4, 5)) == p2);
+    delete p1;
+    delete p2;
 
 }
 
@@ -45,12 +47,13 @@ void ShogiCoreModuleTest::gameLogicTest()
 {
     Board board;
     Piece *p1 = new Piece(Rook, Sente);
-    Piece *p2 = new Piece(King, Sente);
+    Piece *p2 = new Piece(King, Gote);
     ShogiGameLogic g(board);
     board.setPiece(p1, Position(2,2));
-    board.setPiece(p2, Position(5,5));
+    board.setPiece(p2, Position(2,4));
+    QVERIFY(g.checkShah(Gote));
     QVERIFY(g.checkMove(p1,Position(2,4)));
-    QVERIFY(g.checkMove(p2,Position(5,6)));
+    QVERIFY(g.checkMove(p2,Position(3,5)));
     delete p1;
     delete p2;
 
