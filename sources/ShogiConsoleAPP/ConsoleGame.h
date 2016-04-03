398448433b00477ef5cb92f07ca995f647b08ce1
lezhenin <yurij.lezhenin@yandex.ru>
2016-04-03 12:31:59 +0300

    add table of labels

--- sources/ShogiConsoleAPP/ConsoleGame.h
+++ sources/ShogiConsoleAPP/ConsoleGame.h
@@ -24,8 +24,7 @@ private:
     std::map<std::string,int> tableOfCommands = {{"pick",0},{"unpick",1},{"move",2},{"drop",3},{"exit",4}};
     std::map<std::string,PieceType> tableOfTypes = {{"pawn", Pawn},{"king", King}, {"golden general", GoldGeneral}, {"rook" ,Rook},
                                                     {"bishop", Bishop}, {"silver general", SilverGeneral}, {"knight", Knight}, {"lance", Lance}};
-//    std::map<PieceType,std::string> = {King, GoldGeneral, Rook, Bishop, SilverGeneral, Knight, Lance, Pawn,
-//            PromotedRook, PromotedBishop, PromotedSilverGeneral,  PromotedKnight, PromotedLance, PromotedPawn}
+//    std::map<PieceType,std::string> = {{King, K}, {GoldGeneral, G}, {Rook, R}, {Bishop, B}, {SilverGeneral, S}, {Knight, H}, {Lance, L}, {Pawn, P}, {PromotedRook, R}, {PromotedBishop, B}, {PromotedSilverGeneral, S}, {PromotedKnight, H}, {PromotedLance, L}, {PromotedPawn, P}}
 };
 
 #endif //SHOGI_GAME_H
